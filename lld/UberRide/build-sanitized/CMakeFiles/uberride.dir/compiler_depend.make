0f77349fc5c609f58fc3a55c7be89d027b6656db
Saarthak arora <78367762+Saarthak-Arora@users.noreply.github.com>
2026-08-01 15:14:59 +0530

    Delete lld/UberRide/build-sanitized directory

--- lld/UberRide/build-sanitized/CMakeFiles/uberride.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for uberride.
-# This may be replaced when dependencies are built.
--- lld/UberRide/build-sanitized/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/saarthakarora/Documents/System_design/lld/UberRide
-# Build directory: /Users/saarthakarora/Documents/System_design/lld/UberRide/build-sanitized
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test([=[uberride_tests]=] "/Users/saarthakarora/Documents/System_design/lld/UberRide/build-sanitized/uberride_tests")
-set_tests_properties([=[uberride_tests]=] PROPERTIES  _BACKTRACE_TRIPLES "/Users/saarthakarora/Documents/System_design/lld/UberRide/CMakeLists.txt;27;add_test;/Users/saarthakarora/Documents/System_design/lld/UberRide/CMakeLists.txt;0;")
